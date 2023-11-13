df27836a9a056ff7e302ffcc9cc438fc7fde2290
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2023-11-13 13:56:06 -0500

    player movement in a box

---
+++ scripts/player_magnet.gd
@@ -0,0 +1,56 @@
+extends Sprite2D
+
+var playerPositions = [
+	Vector2(0, -200),
+	Vector2(200, 0),
+	Vector2(0, 200),
+	Vector2(-200, 0),
+]
+
+enum playerPositionStates {
+	TOP = 0,
+	RIGHT = 1,
+	BOTTOM = 2,
+	LEFT = 3,
+}
+
+var currentPlayerPositionState = playerPositionStates.TOP
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.position = playerPositions[currentPlayerPositionState]
+
+
+func _input(event):
+	if event.is_action_pressed("moveClockwise"):
+		# CHANGE STATE CLOCKWISE
+		match currentPlayerPositionState:
+			playerPositionStates.TOP:
+				# go to right
+				currentPlayerPositionState = playerPositionStates.RIGHT
+			playerPositionStates.RIGHT:
+				# go to bottom
+				currentPlayerPositionState = playerPositionStates.BOTTOM
+			playerPositionStates.BOTTOM:
+				# go to left
+				currentPlayerPositionState = playerPositionStates.LEFT
+			playerPositionStates.LEFT:
+				# go to top
+				currentPlayerPositionState = playerPositionStates.TOP
+	elif event.is_action_pressed("moveCounterClockwise"):
+		# CHANGE STATE COUNTERCLOCKWISE
+		match currentPlayerPositionState:
+			playerPositionStates.TOP:
+				# go to left
+				currentPlayerPositionState = playerPositionStates.LEFT
+			playerPositionStates.RIGHT:
+				# go to top
+				currentPlayerPositionState = playerPositionStates.TOP
+			playerPositionStates.BOTTOM:
+				# go to right
+				currentPlayerPositionState = playerPositionStates.RIGHT
+			playerPositionStates.LEFT:
+				# go to bottom
+				currentPlayerPositionState = playerPositionStates.BOTTOM
+	# ACTUALLY DO THE MOVEMENT
+	self.position = playerPositions[currentPlayerPositionState]
